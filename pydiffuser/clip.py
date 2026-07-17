b8d224d1f352d069fca25c5ff43ed7f8edccda3b
Sam Ireland <mail@samireland.com>
2026-07-17 12:40:10 +0100

    Initial CLIP tokenizer functions

---
+++ pydiffuser/clip.py
@@ -0,0 +1,67 @@
+import json
+import csv
+import re
+import torch
+import safetensors
+from transformers import CLIPTokenizer
+
+MAX_LENGTH = 77
+
+
+def tokenize(
+    text: str,
+    clip_tokenizer: CLIPTokenizer = None,
+) -> tuple[list[list[int]], list[list[tuple[str, int]]]]:
+    if not clip_tokenizer:
+        tokenizer_path = "data/clip_tokenizer"
+        clip_tokenizer = CLIPTokenizer.from_pretrained(tokenizer_path)
+    tokens = _text_to_tokens(text, clip_tokenizer)
+    tokens = _break_up_tokens(tokens, clip_tokenizer)
+    mappings = _create_token_string_mapping(tokens, clip_tokenizer)
+    return tokens, mappings
+
+
+def _text_to_tokens(text: str, clip_tokenizer: CLIPTokenizer) -> list[int]:
+    """Creates a list of tokens IDs from the given text. It is a single flat
+    list regardless of the length of the text, and the start and end tokens are
+    removed."""
+
+    all_tokens = clip_tokenizer.encode(text)
+    if all_tokens[0] == clip_tokenizer.bos_token_id:
+        all_tokens.pop(0)
+    if all_tokens[-1] == clip_tokenizer.eos_token_id:
+        all_tokens.pop(-1)
+    return all_tokens
+
+
+def _break_up_tokens(
+    tokens: list[int],
+    clip_tokenizer: CLIPTokenizer,
+    max_length: int = MAX_LENGTH,
+) -> list[list[int]]:
+    """Breaks a list of tokens into a list of lists of tokens, where each
+    sublist is of length max_length, and the start and end tokens are added."""
+
+    bos = clip_tokenizer.bos_token_id
+    eos = clip_tokenizer.eos_token_id
+    pad = clip_tokenizer.pad_token_id
+    tokens = [
+        tokens[i : i + max_length - 2] for i in range(0, len(tokens), max_length - 2)
+    ]
+    tokens = [[bos] + t + [eos] for t in tokens]
+    if len(tokens[-1]) < max_length:
+        tokens[-1].pop(-1)
+        tokens[-1] += [pad] * (max_length - len(tokens[-1]))
+    return tokens
+
+
+def _create_token_string_mapping(
+    tokens: list[int], clip_tokenizer: CLIPTokenizer
+) -> list[list[tuple[str, int]]]:
+    """Creates a mapping of token values to token integers."""
+
+    mappings = []
+    for sub_list in tokens:
+        strings = clip_tokenizer.convert_ids_to_tokens(sub_list)
+        mappings.append([(s, t) for t, s in zip(sub_list, strings)])
+    return mappings
---
+++ tests/unit/test_clip.py
@@ -0,0 +1,163 @@
+import os
+from unittest import TestCase
+from unittest.mock import patch, mock_open, call, Mock
+from transformers import CLIPTokenizer
+from pydiffuser.clip import (
+    tokenize,
+    _text_to_tokens,
+    _break_up_tokens,
+    _create_token_string_mapping,
+)
+
+
+class TokenizeTests(TestCase):
+
+    @patch("pydiffuser.clip._text_to_tokens")
+    @patch("pydiffuser.clip._break_up_tokens")
+    @patch("pydiffuser.clip._create_token_string_mapping")
+    def test_tokenize_custom_tokenizer(
+        self,
+        mock_create_mapping,
+        mock_break_up,
+        mock_to_tokens,
+    ):
+        mock_create_mapping.return_value = [
+            [("the", 599)],
+            [("big", 1915), ("one", 1980)],
+            [("is", 595), ("coming", 14916)],
+        ]
+        tokenizer = Mock()
+        tokens, mappings = tokenize("A photo of a cat.", tokenizer)
+
+        mock_to_tokens.assert_called_once_with("A photo of a cat.", tokenizer)
+        mock_break_up.assert_called_once_with(mock_to_tokens.return_value, tokenizer)
+        mock_create_mapping.assert_called_once_with(
+            mock_break_up.return_value, tokenizer
+        )
+        self.assertEqual(tokens, mock_break_up.return_value)
+        self.assertEqual(mappings, mock_create_mapping.return_value)
+
+    @patch("pydiffuser.clip.CLIPTokenizer.from_pretrained")
+    @patch("pydiffuser.clip._text_to_tokens")
+    @patch("pydiffuser.clip._break_up_tokens")
+    @patch("pydiffuser.clip._create_token_string_mapping")
+    def test_tokenize_default_tokenizer(
+        self,
+        mock_create_mapping,
+        mock_break_up,
+        mock_to_tokens,
+        mock_from_pretrained,
+    ):
+        mock_create_mapping.return_value = [
+            [("the", 599)],
+            [("big", 1915), ("one", 1980)],
+            [("is", 595), ("coming", 14916)],
+        ]
+        tokenize("A photo of a cat.")
+        mock_from_pretrained.assert_called_once_with("data/clip_tokenizer")
+        mock_to_tokens.assert_called_once_with(
+            "A photo of a cat.", mock_from_pretrained.return_value
+        )
+        mock_break_up.assert_called_once_with(
+            mock_to_tokens.return_value, mock_from_pretrained.return_value
+        )
+        mock_create_mapping.assert_called_once_with(
+            mock_break_up.return_value, mock_from_pretrained.return_value
+        )
+
+
+class TextToTokensTests(TestCase):
+
+    def test_text_to_tokens(self):
+        prompt = "A photo of a cat."
+        tokenizer = CLIPTokenizer.from_pretrained(
+            os.path.join(os.getcwd(), "pydiffuser", "data", "clip_tokenizer")
+        )
+        tokens = _text_to_tokens(prompt, tokenizer)
+        self.assertEqual(tokens, [320, 1125, 539, 320, 2368, 269])
+
+
+class BreakUpTokensTests(TestCase):
+
+    def test_short_list(self):
+        tokens = [1, 2, 3, 4, 5, 10, 11, 12, 13, 14, 15]
+        tokenizer = CLIPTokenizer.from_pretrained(
+            os.path.join(os.getcwd(), "pydiffuser", "data", "clip_tokenizer")
+        )
+        result = _break_up_tokens(tokens, tokenizer, max_length=20)
+        self.assertEqual(
+            result,
+            [
+                [
+                    49406,
+                    1,
+                    2,
+                    3,
+                    4,
+                    5,
+                    10,
+                    11,
+                    12,
+                    13,
+                    14,
+                    15,
+                    49407,
+                    49407,
+                    49407,
+                    49407,
+                    49407,
+                    49407,
+                    49407,
+                    49407,
+                ]
+            ],
+        )
+
+    def test_list_of_length_max_length(self):
+        tokens = [1, 2, 3, 4, 5, 10, 11, 12, 13, 14, 15]
+        tokenizer = CLIPTokenizer.from_pretrained(
+            os.path.join(os.getcwd(), "pydiffuser", "data", "clip_tokenizer")
+        )
+        result = _break_up_tokens(tokens, tokenizer, max_length=13)
+        self.assertEqual(
+            result, [[49406, 1, 2, 3, 4, 5, 10, 11, 12, 13, 14, 15, 49407]]
+        )
+
+    def test_list_longer_than_max_length(self):
+        tokens = [1, 2, 3, 4, 5, 10, 11, 12, 13, 14, 15]
+        tokenizer = CLIPTokenizer.from_pretrained(
+            os.path.join(os.getcwd(), "pydiffuser", "data", "clip_tokenizer")
+        )
+        result = _break_up_tokens(tokens, tokenizer, max_length=8)
+        self.assertEqual(
+            result,
+            [
+                [49406, 1, 2, 3, 4, 5, 10, 49407],
+                [49406, 11, 12, 13, 14, 15, 49407, 49407],
+            ],
+        )
+
+
+class CreateTokenStringMappingTests(TestCase):
+
+    def test_map_tokens_to_strings_single_list(self):
+        tokens = [[599, 1915, 1980]]
+        tokenizer = CLIPTokenizer.from_pretrained(
+            os.path.join(os.getcwd(), "pydiffuser", "data", "clip_tokenizer")
+        )
+        result = _create_token_string_mapping(tokens, tokenizer)
+        self.assertEqual(result, [[("the", 599), ("big", 1915), ("one", 1980)]])
+
+    def test_map_tokens_to_strings_multiple_lists(self):
+        tokens = [[599, 1915, 1980], [595, 14916]]
+        tokenizer = CLIPTokenizer.from_pretrained(
+            os.path.join(os.getcwd(), "pydiffuser", "data", "clip_tokenizer")
+        )
+        result = _create_token_string_mapping(tokens, tokenizer)
+        self.assertEqual(
+            result,
+            [
+                [("the", 599), ("big", 1915), ("one", 1980)],
+                [("is", 595), ("coming", 14916)],
+            ],
+        )

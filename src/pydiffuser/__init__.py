f1b3e8ab77193770863e56eaf07f9e09faeff411
Sam Ireland <mail@samireland.com>
2026-08-16 15:11:35 +0100

    Set version in pyproject

--- src/pydiffuser/__init__.py
+++ src/pydiffuser/__init__.py
@@ -1,2 +1,5 @@
-__version__ = "0.1.0"
-__author__ = "Sam Ireland"
+from email.utils import parseaddr
+from importlib.metadata import metadata, version
+
+__version__ = version("pydiffuser")
+__author__ = parseaddr(metadata("pydiffuser")["Author-email"])[0]
